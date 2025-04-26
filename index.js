93858618f46585e2cb0bd8c06f666da48dd408d0
MMansyy <hessenmansyy@gmail.com>
2025-04-26 19:49:57 +0300

    Comment out server listening logic in index.js

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ app.use('*', (req, res) => {
 app.use(globalErrorHandel)
 DBConnection();
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
 
 
 export default app;
