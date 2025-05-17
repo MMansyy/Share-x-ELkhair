7507e00c2f82042b9e1c751b00642376ffc00178
MMansyy <hessenmansyy@gmail.com>
2025-05-17 15:52:26 +0300

    Reorder user routes for improved clarity; move getUser and deleteUser routes to the end

--- src/Modules/User/User.routes.js
+++ src/Modules/User/User.routes.js
@@ -7,17 +7,17 @@ import uploadSingle from '../../../utils/multerCloud.js';
 const UserRoutes = express.Router();
 
 UserRoutes.get('/', protectedRoutes, getMe);
-UserRoutes.get('/:id', getUser);
 UserRoutes.put('/', protectedRoutes, updateUser)
 UserRoutes.put('/updatepassword', protectedRoutes, updatePassword)
 UserRoutes.put('/profilePicture', protectedRoutes, uploadSingle, updateProfilePicture);
 UserRoutes.get('/restaurants', getRestaurants);
 UserRoutes.get('/charites', getCharites);
 UserRoutes.get('/allusers', protectedRoutes, allowTo('admin'), getAllUsers);
-UserRoutes.delete('/:id' , protectedRoutes, allowTo('admin') , deleteUser);
-UserRoutes.put('/forgetpassword', forgotPassword )
-UserRoutes.put('/verifyotp' , verifyOtp);
-UserRoutes.put('/resetpassword' , resetPassword)
+UserRoutes.put('/forgetpassword', forgotPassword)
+UserRoutes.put('/verifyotp', verifyOtp);
+UserRoutes.put('/resetpassword', resetPassword)
+UserRoutes.get('/:id', getUser);
+UserRoutes.delete('/:id', protectedRoutes, allowTo('admin'), deleteUser);
 
 
 
