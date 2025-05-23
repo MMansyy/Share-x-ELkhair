1656f3125f734c715a7a49fdc209648fb6b94c90
MMansyy <hessenmansyy@gmail.com>
2025-05-24 02:35:47 +0300

    Fix import paths in Notification routes for consistency

--- src/Modules/Notfication/Notfiaction.routes.js
+++ src/Modules/Notfication/Notfiaction.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { getNotifications } from './Notfication.controller';
-import { protectedRoutes } from '../Auth/Auth.controller';
+import { getNotifications } from './Notfication.controller.js';
+import { protectedRoutes } from '../Auth/Auth.controller.js';
 
 
 const NotificationRouter = express.Router();
