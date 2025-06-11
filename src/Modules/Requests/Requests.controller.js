4b8c1222117b67c25c11ff94779cb12b4eca82fe
MMansyy <hessenmansyy@gmail.com>
2025-06-11 03:34:35 +0300

    Refactor deleteRequest to delete notifications by requestID and streamline error handling

--- src/Modules/Requests/Requests.controller.js
+++ src/Modules/Requests/Requests.controller.js
@@ -171,10 +171,7 @@ export const deleteRequest = asyncHandler(async (req, res, next) => {
     if (request.charityID._id.toString() !== req.user.id && req.user.role !== "admin") {
         return next(new AppError("You are not authorized to delete this request", 403));
     }
-    const deleteNotifications = await notificationModel.deleteMany({ donationID: request.donationID._id });
-    if (deleteNotifications.deletedCount === 0) {
-        return next(new AppError("No notifications found to delete", 404));
-    }
+    const deleteNotifications = await notificationModel.deleteMany({ requestID });
     await requestModel.findByIdAndDelete(requestID);
     return res.status(200).json({ success: true, data: {} });
 });
